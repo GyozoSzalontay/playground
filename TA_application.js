9f2ea5efa790cf483244aa89f6726527ef874501
Gyozo Szalontay <gyozo.szalontay@gmail.com>
2021-11-28 23:11:50 +0100

    get the solution

--- TA_application.js
+++ TA_application.js
@@ -1,20 +1,20 @@
 console.log("hello")
-const my_sentence = "Hello bello, szuper egy nap"
+const my_sentence = "Change your life learn to code"
 
 function spin_words(sentence){
   let words = sentence.split(" ");
-  let newCamelArrey = [];
-    words.map((i) => {
+  let newSentenceArrey = [];
+    words.map((word) => {
       let newWord;
-      if (i.length >= 5){
-        newWord = i.split("").reverse().join("")}
+      if (word.length >= 5){
+        newWord = word.split("").reverse().join("")}
       else {
-        newWord = i
+        newWord = word
       }
-      newCamelArrey.push(newWord);
+      newSentenceArrey.push(newWord);
     });
-    console.log(newCamelArrey.join(" "));
-    return newCamelArrey.join(" ");
+    console.log(newSentenceArrey.join(" "));
+    return newSentenceArrey.join(" ");
   }
 
     spin_words(my_sentence);
